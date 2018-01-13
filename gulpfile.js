e96b1707fa1a23fe55effb9eba2c720c00bdd6ed
VuongNguyen <ndvuong1996@gmail.com>
2018-01-13 21:39:33 +0700

    [LE-1] init structure

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')({
+	overridePattern: true,
+	pattern: ['gulp-*', 'gulp.*', '@*/gulp{-,.}*', 'browser-*']
+});
+
+// Error handler
+var onError = function (err) {
+	this.emit('end');
+};
+
+// Set default tasks work with html and css files
+gulp.task('default', function() {
+
+	// place code for your default task here
+	gulp.src('src/app.js')
+		.pipe(plugins.babel({
+			presets: ['env']
+		}))
+		.on('error', onError)
+		.pipe(gulp.dest('dist'));
+
+	// verify code style in dist folder
+	gulp.src('src/**/*.js')
+		.pipe(plugins.jshint(''))
+		.pipe(plugins.jshint.reporter('jshint-stylish'));
+
+	gulp.src('src/**/*.html')
+	    .pipe(plugins.htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist'));
+		
+	gulp.src(['src/**/style.scss', 'src/**/bootstrap-custom.scss'])
+		.pipe(plugins.sass())
+		.pipe(gulp.dest('dist'));
+
+});
+
+// Minify images
+gulp.task('minifyimage', function() {
+	gulp.src('src/images/*')
+		.pipe(plugins.imagemin())
+		.pipe(gulp.dest('dist/images'));
+
+});
+
+// gulp serve and livereload
+gulp.task('serve', ['minifyimage'], function() {
+	plugins.browserSync.init({
+        server: "./dist"
+    });
+
+	gulp.watch(['src/**/*.js', 'src/**/*.html', 'src/**/*.scss'], ['default'])
+		.on('change', plugins.browserSync.reload);
+	gulp.watch(['src/images/*'], ['minifyimage'])
+		.on('change', plugins.browserSync.reload);
+});
