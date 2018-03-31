6718dfef64f270dd8344e6e5eb3f63e611da60fa
VuongNguyen <ndvuong1996@gmail.com>
2018-03-31 12:30:54 +0700

    [LE] Implement social and sub-menu

--- src/app.js
+++ src/app.js
@@ -2,3 +2,9 @@ function switchView () {
     $('.products-list').toggleClass('grid-view row-view');
     $('.switch-view').toggleClass('disabled');
 }
+
+function openProductDetails() {
+    $('.btn-details').toggleClass(['open-product']);
+    $('.product-content').toggle();
+    $('.product-details').toggleClass('product-details-block');
+}
